ea91a0383c523fe91655560d97649d179ef9fc9c
devanjal <devanjal_kotia@yahoo.co.in>
2018-03-22 17:21:39 -0700

    minor change

--- StackDriverDemo/src/main/java/com/stackdriver/ErrorReport.java
+++ StackDriverDemo/src/main/java/com/stackdriver/ErrorReport.java
@@ -8,24 +8,20 @@ import java.util.Map;
 import org.fluentd.logger.FluentLogger;
 
 public class ErrorReport {
-	private static FluentLogger LOGGER = FluentLogger.getLogger("errorReport");
+	private static FluentLogger LOGGER = FluentLogger.getLogger(ErrorReport.class.getName());
 	public static void main(String[] args) {
 
 		try {
 			throw new Exception("General Error");
 		}catch(Exception e) {
 			StringWriter exceptionWriter = new StringWriter();
-		    e.printStackTrace(new PrintWriter(exceptionWriter));
-			Map<String, Object> errorData = new HashMap<String, Object>();
-			errorData.put("message", exceptionWriter.toString());
-			LOGGER.log("error", errorData);
+//		    e.printStackTrace(new PrintWriter(exceptionWriter));
+//			Map<String, Object> errorData = new HashMap<String, Object>();
+//			errorData.put("message", exceptionWriter.toString());
+//			LOGGER.log("error", errorData);
+			LOGGER.log("error",exceptionWriter.toString() , ErrorReport.class.getName());
 		}
-		Map<String, Object> data = new HashMap<String, Object>();
-	    data.put("message", "Some Logger");
-	    Map<String,String> serviceContextData = new HashMap<String,String>();
-	    serviceContextData.put("service", "errorReport");
-	    data.put("serviceContext", serviceContextData);
-	    LOGGER.log("info", data);
+	    LOGGER.log("info", "Simple Entry", ErrorReport.class.getName());
 	    
 
 	}
